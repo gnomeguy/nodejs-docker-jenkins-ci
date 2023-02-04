0914f9947564b16dd13e7da7460195c6863e407e
gnomeguy <79591381+gnomeguy@users.noreply.github.com>
2023-02-04 06:24:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
-require('dotenv').config();
-const http = require('http');
-const uuidv4 = require('uuid/v4');
-const faker = require('faker');
-
-const port = process.env.SECRET_AGENT_PORT || 3050;
-const agent = process.env.SECRET_AGENT || 'Unknown';
-
-const getMessage = ()=> {
-    const msg = {}
-    msg.secretAgent = agent;
-    msg.secretMessage = faker.lorem.words(5);
-    msg.id = uuidv4();
-    msg.created = new Date();
-
-    return msg;
-}
-
-const handleRequest = (request, response)  => {
-    response.setHeader("Content-Type", "application/json");
-    response.setHeader("x-correlation-id", uuidv4());
-
-    const str = JSON.stringify(getMessage(), null, 4);
-    response.writeHead(200);
-    response.end(str);
-    console.log(str);
-};
-
-const server = http.createServer(handleRequest);
-
-server.listen(port, () => {
-    console.log(`Secret Agent Server is listening on port ${port} at ${new Date()}`);
-});
-
-const shutdown = () => {
-    console.log(`Secret Agent Server is shutting down at ${new Date()}`);
-    server.close();
-};
-
-module.exports = {server,shutdown};
+//importing node framework
+var express = require(‘express’);
+ 
+var app = express();//Respond with "hello world" for requests that hit our root "/"
+app.get(‘/’, function (req, res) {
+ res.send(‘hello world’);
+});//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+ 
+module.exports = app;
