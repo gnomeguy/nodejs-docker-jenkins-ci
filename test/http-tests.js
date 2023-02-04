29919f91c329147a28244e5e2c6ab6082d2be3df
gnomeguy <79591381+gnomeguy@users.noreply.github.com>
2023-02-04 06:24:03 +0000

    Update http-tests.js

--- test/http-tests.js
+++ test/http-tests.js
@@ -1,34 +1,6 @@
-'use strict';
-const supertest = require('supertest');
-const chai = require('chai');
-const expect = require('chai').expect;
-const describe = require('mocha').describe;
-const it = require('mocha').it;
-
-const {server,shutdown} = require('../index');
-
-describe('HTTP Tests: ', () => {
-    after(function () {
-        shutdown();
-    });
-
-    it('Can access GET basic information', function(done){
-        //Go get all the lists
-        supertest(server)
-            .get('/')
-            .set('Accept', 'application/json')
-            .then((res) => {
-                expect(res.body).to.be.an('object');
-                console.log('Object check OK')
-                expect(res.body.secretAgent).to.be.an('string');
-                console.log('secretAgent check OK')
-                expect(res.body.secretMessage).to.be.an('string');
-                console.log('secretMessage check OK')
-                expect(res.body.id).to.be.an('string');
-                console.log('id equality check OK')
-                done();
-            })
-            .catch(done);
-    });
-
+var request = require(‘supertest’);
+var app = require(‘../index.js’);describe(‘GET /’, function() {
+ it(‘respond with hello world’, function(done) { //navigate to root and check the the response is "hello world"
+ request(app).get(‘/’).expect(‘hello world’, done);
+ });
 });
